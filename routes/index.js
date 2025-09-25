ecb598eff5e60b97213a30b3b1b0e60963590eaa
octaviobottari <octaviobottari7@gmail.com>
2025-09-25 18:06:36 -0300

    login

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
   res.send('Hello World');
 });
+router.get('/login', (req, res) => {
+  res.redirect('/auth/github');
+});
 router.get('/logout', (req, res) => {
   req.logout(function(err) {
     if (err) return res.status(500).json({ error: 'Logout failed' });
