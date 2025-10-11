7e94c2e88fb4de6d5d3bee0af1fcab54fe11a380
octaviobottari <octaviobottari7@gmail.com>
2025-10-11 13:38:45 -0300

    update

--- models/user.js
+++ models/user.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false }, 
+  githubId: { type: String, unique: true, sparse: true }, 
+  githubProfile: { type: Object }, 
   createdAt: { type: Date, default: Date.now }
 });
 
